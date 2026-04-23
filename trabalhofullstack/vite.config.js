2b1f2525edbc0b174cf8b23d63c27ea35b182655
Andromeda-Queen <l.alves23132@gmail.com>
2026-04-23 00:32:44 -0300

    1.3

--- trabalhofullstack/vite.config.js
+++ trabalhofullstack/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Trabalho-1-fullstack/', // Adicione esta linha
   plugins: [react()],
 })
